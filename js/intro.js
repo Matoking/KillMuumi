eaac76b3c4442bc58da0d556803ececaada3a71c
Janne Pulkkinen <jannepulk@gmail.com>
2016-01-15 18:21:57 +0200

    Tilanvaihto intron ja pelin välillä

--- js/intro.js
+++ js/intro.js
@@ -1,7 +1,26 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+KillMuumi.IntroState = function() {};
 
+KillMuumi.IntroState.prototype = {
+    /*
+     * Peli luodaan tiedostojen lataamisen jälkeen tässä
+     */
+    create: function() {
+        this.text = this.add.text(this.world.centerX, 
+                                  this.world.centerY, 
+                                  "KillMuumi\nVuosisadan Muumitapposimulaattori",
+                                {
+                                    font: "24px",
+                                    align: "center",
+                                    color: "white"
+                                });
+    },
 
+    /*
+     * Peliä päivitetään n. 60 kertaa sekunnissa tässä
+     */
+    update: function() {
+        if (this.input.keyboard.isDown(Phaser.KeyCode.X)) {
+            startGame();
+        }
+    }
+};
