651508bf21361dd10cea0f5978dd1c2b8913eda9
Janne Pulkkinen <jannepulk@gmail.com>
2016-01-15 17:56:15 +0200

    Nyt liikkuu

--- js/game.js
+++ js/game.js
@@ -18,16 +18,33 @@ function preload() {
  * Peli luodaan tiedostojen lataamisen jälkeen tässä
  */
 function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
     game.state.backgroundColor = "#55FFAA";
     
     player = game.add.sprite(40, 40, "testiKuva");
+    game.physics.enable(player, Phaser.Physics.ARCADE);
 }
 
 /*
  * Peliä päivitetään n. 60 kertaa sekunnissa tässä
  */
 function update() {
+    if (game.input.keyboard.isDown(Phaser.KeyCode.LEFT)) {
+        player.body.velocity.x = -80;
+    } else if (game.input.keyboard.isDown(Phaser.KeyCode.RIGHT)) {
+        player.body.velocity.x = 80;
+    } else {
+        player.body.velocity.x = 0;
+    }
     
+    if (game.input.keyboard.isDown(Phaser.KeyCode.UP)) {
+        player.body.velocity.y = -80;
+    } else if (game.input.keyboard.isDown(Phaser.KeyCode.DOWN)) {
+        player.body.velocity.y = 80;
+    } else {
+        player.body.velocity.y = 0;
+    }
 }
 
 /*
