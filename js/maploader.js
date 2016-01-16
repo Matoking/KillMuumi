1f7a801991b0f960855360a703c72d3f1935acf2
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2016-01-16 15:43:30 +0200

    maploaderii

--- js/maploader.js
+++ js/maploader.js
@@ -29,40 +29,150 @@ MapLoader.prototype = {
 
             switch (gameObject.type) {
                 case "kello":
-                    var clock = game.make.sprite(objectX, objectY, "kello");
+                    var kello = game.make.sprite(objectX, objectY, "kello");
 
-                    clock.animations.add("idle", [0, 1, 2, 3, 4, 5], 8, true);
-                    clock.animations.play("idle");
+                    kello.animations.add("idle", [0, 1, 2, 3, 4, 5], 8, true);
+                    kello.animations.play("idle");
 
-                    state.levelObstacles.add(clock);
+                    state.levelObstacles.add(kello);
 
-                    clock.body.allowGravity = false;
-                    clock.body.immovable = true;
+                    kello.body.allowGravity = false;
+                    kello.body.immovable = true;
 
-                    clock.body.checkCollision = {
+                    kello.body.checkCollision = {
                         up: true,
                         left: false,
                         right: false,
                         down: false
                     };
 
-                    console.log("Clock created at " + objectX + "x" + objectY);
-                    
                 case "hella":
                     var hella = game.make.sprite(objectX, objectY, "hella");
 
                     state.levelObstacles.add(hella);
 
-                    clock.body.allowGravity = false;
-                    clock.body.immovable = true;
+                    hella.body.allowGravity = false;
+                    hella.body.immovable = true;
 
-                    clock.body.checkCollision = {
+                    hella.body.checkCollision = {
                         up: true,
                         left: false,
                         right: false,
                         down: false
                     };
+
+                case "amme":
+                    var amme = game.make.sprite(objectX, objectY, "amme");
+
+                    state.levelObstacles.add(amme);
+
+                    amme.body.allowGravity = false;
+                    amme.body.immovable = true;
+
+                    amme.body.checkCollision = {
+                        up: true,
+                        left: false,
+                        right: false,
+                        down: false
+                    };
+
+                case "keittiokaappi":
+                    var keittiokaappi = game.make.sprite(objectX, objectY, "keittiokaappi");
+
+                    state.levelObstacles.add(keittiokaappi);
+
+                    keittiokaappi.body.allowGravity = false;
+                    keittiokaappi.body.immovable = true;
+
+                    keittiokaappi.body.checkCollision = {
+                        up: true,
+                        left: false,
+                        right: false,
+                        down: false
+                    };
+
+                case "kirjahylly":
+                    var kirjahylly = game.make.sprite(objectX, objectY, "kirjahylly");
+
+                    state.levelObstacles.add(kirjahylly);
+
+                    kirjahylly.body.allowGravity = false;
+                    kirjahylly.body.immovable = true;
+
+                    kirjahylly.body.checkCollision = {
+                        up: true,
+                        left: false,
+                        right: false,
+                        down: false
+                    };
+
+                case "laatikko":
+                    var laatikko = game.make.sprite(objectX, objectY, "laatikko");
+
+                    state.levelObstacles.add(kirjahylly);
+
+                    laatikko.body.allowGravity = false;
+                    laatikko.body.immovable = true;
+
+                    laatikko.body.checkCollision = {
+                        up: true,
+                        left: false,
+                        right: false,
+                        down: false
+                    };
+
+                case "pesukone":
+                    var pesukone = game.make.sprite(objectX, objectY, "pesukone");
+
+                    state.levelObstacles.add(pesukone);
+
+                    pesukone.body.allowGravity = false;
+                    pesukone.body.immovable = true;
+
+                    pesukone.body.checkCollision = {
+                        up: true,
+                        left: false,
+                        right: false,
+                        down: false
+                    };
+                case "poyta":
+                    var poyta = game.make.sprite(objectX, objectY, "poyta");
+
+                    state.levelObstacles.add(poyta);
+
+                    poyta.body.allowGravity = false;
+                    poyta.body.immovable = true;
+
+                    poyta.body.checkCollision = {
+                        up: true,
+                        left: false,
+                        right: false,
+                        down: false
+                    };
+
+
+                case "sanky":
+                    var sanky = game.make.sprite(objectX, objectY, "sanky");
+
+                    state.levelObstacles.add(sanky);
+
+                    sanky.body.allowGravity = false;
+                    sanky.body.immovable = true;
+
+                    sanky.body.checkCollision = {
+                        up: true,
+                        left: false,
+                        right: false,
+                        down: false
+                    };
+
             }
         }
     }
 };
+
+
+
+
+
+                        
